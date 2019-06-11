7293a6242fdf27b2c056992d231518446df24a12
heather zurek and lindsey baker <lindsey@>
2019-06-11 10:55:14 -0700

    add property bindings to show ratings

--- to-do/src/app/models/task.model.ts
+++ to-do/src/app/models/task.model.ts
@@ -1,4 +1,4 @@
 export class Task {
   public done: boolean = false;
-  constructor(public title: string, public ingredients: string, public directions: string) { }
+  constructor(public title: string, public ingredients: string, public directions: string, public rating: number) { }
 }
